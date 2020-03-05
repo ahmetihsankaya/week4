15b09a4075fe171775148ff75020b3f63d369080
AIK <ahmetihsankaya@gmail.com>
2020-03-05 18:36:31 +0100

    week 4

---
+++ deneme.py
@@ -0,0 +1,22 @@
+import turtle
+window = turtle.Screen()
+window.bgcolor("lightgreen")
+tess = turtle.Turtle()
+tess.shape("turtle")
+tess.color("blue")
+
+for _ in range(12):
+    tess.stamp()
+    tess.penup()
+    tess.forward(100)
+    tess.pendown()
+    tess.forward(20)
+    tess.penup()
+    tess.forward(20)
+    tess.stamp()
+
+tess.clear()
+
+
+window.exitonclick()
+
